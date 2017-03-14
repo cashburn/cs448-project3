aad9f98951641e36ff17069ef81fd8c07c6c6353
Colin Ashburn <cashburn@purdue.edu>
2017-03-14 17:30:10 -0400

    Skeleton compilation

--- RelOperator/src/relop/SortMergeJoin.java
+++ RelOperator/src/relop/SortMergeJoin.java
@@ -1,6 +1,32 @@
 package relop;
 
 public class SortMergeJoin extends Iterator {
+    public SortMergeJoin(Iterator left, Iterator right, Integer lcol, Integer rcol) {
+            
+    }
+    
+    public void close() {
+        //TODO: Implement close    
+    }
 
+    public void explain(int depth) {
+        //TODO: Implement explain
+    }
+
+    public Tuple getNext() {
+        return new Tuple(new Schema(1));    
+    }
+    
+    public boolean hasNext() {
+        return true;
+    }
+
+    public boolean isOpen() {
+        return true;    
+    }
+
+    public void restart() {
+        //TODO: Restart    
+    }
 	
 }
